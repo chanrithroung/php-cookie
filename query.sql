19ea39abd175d83055dd3cb7b543fae5e691c129
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2024-12-19 17:12:22 +0700

    first Commit

--- query.sql
+++ query.sql
@@ -3,6 +3,6 @@ create table post(
     post_id int AUTO_INCREMENT PRIMARY key,
     title varchar(101), 
     author int NOT NULL, 
-    status VARCHAR(1),
+    status VARCHAR(1) DEFAULT '1',
     thumbnail varchar(255) 
 );
